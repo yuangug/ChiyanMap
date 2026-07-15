c79c8fc827da688612250d2d2a9705adef87d007
yuangug <526184353@qq.com>
2026-07-15 12:51:17 +0800

    fix: 圆形地图旋转使用相对索引
    D3D11 DrawIndexed 会把 BaseVertexLocation 加到每个索引上，
    所以 PrimWriteIdx 必须写相对值 (0,1,2...) 而非绝对地址。
    之前用 baseIdx+offset 导致 V+V 越界到垃圾数据。

--- src/hooks/DX11Hook.h
+++ src/hooks/DX11Hook.h
@@ -1022,19 +1022,19 @@ namespace DX11Hook {
                 const int segs = 48;
                 draw_list->PrimReserve(segs * 3, segs + 2);
                 ImVec2 ctr_uv((uv0.x + uv1.x) * 0.5f, (uv0.y + uv1.y) * 0.5f);
-                unsigned int centerIdx = draw_list->_VtxCurrentIdx;
+                unsigned int centerIdx = 0;
                 draw_list->PrimWriteVtx(ImVec2(cx, cy), ctr_uv, IM_COL32_WHITE);
                 unsigned int prevIdx = 0;
                 for (int i = 0; i <= segs; i++) {
                     float a = (float)i / (float)segs * 2.0f * 3.14159265f;
                     float u_rel = 0.5f + 0.5f * cos(a + yawRad);
                     float v_rel = 0.5f + 0.5f * sin(a + yawRad);
-                    unsigned int curIdx = draw_list->_VtxCurrentIdx;
                     draw_list->PrimWriteVtx(
                         ImVec2(cx + IM_MAP_R * cos(a), cy + IM_MAP_R * sin(a)),
                         ImVec2(uv0.x + (uv1.x - uv0.x) * u_rel, uv0.y + (uv1.y - uv0.y) * v_rel),
                         IM_COL32_WHITE
                     );
+                    unsigned int curIdx = 1 + i;
                     if (i > 0) {
                         draw_list->PrimWriteIdx((ImDrawIdx)(int)centerIdx);
                         draw_list->PrimWriteIdx((ImDrawIdx)(int)prevIdx);
